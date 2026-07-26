631813fe1b8e1b68f747ab788ce9492ace5854af
Anthony Cavuoti <anthonycavuoti@gmail.com>
2026-07-26 16:23:01 -0700

    Improve the locomtion feel

--- overworld/player/playerController.gd
+++ overworld/player/playerController.gd
@@ -1,20 +1,75 @@
 extends CharacterBody3D
 
+@export_group("Movement")
 @export_range(1.0, 10.0) var move_speed := 4.0
+
+@export_group("Turning")
+## Radius of normal moving turns, measured in meters. Lower values turn tighter.
+@export_range(0.1, 5.0, 0.05, "or_greater") var turn_radius := 0.75 
+## Rotation speed used while the character is stopped and turning in place.
+@export_range(45.0, 720.0, 1.0, "degrees") var turn_in_place_speed := 970.0
+## Direction changes at or above this angle trigger a turn in place.
+@export_range(91.0, 179.0, 1.0, "degrees") var turn_in_place_angle := 91.0
 
 @onready var visual: Node3D = $Visual
 
+var is_turning_in_place := false
 
-func _physics_process(_delta: float) -> void:
+
+func _physics_process(delta: float) -> void:
 	var input_vector := _get_input_vector()
 	var move_direction := _screen_input_to_world(input_vector)
-	velocity.x = move_direction.x * move_speed
-	velocity.z = move_direction.z * move_speed
+	var input_strength := input_vector.length()
+
+	velocity.x = 0.0
+	velocity.z = 0.0
 	velocity.y = 0.0
-	move_and_slide()
 
 	if not move_direction.is_zero_approx():
-		visual.rotation.y = atan2(-move_direction.x, -move_direction.z)
+		_steer_toward(move_direction, input_strength, delta)
+	else:
+		is_turning_in_place = false
+
+	move_and_slide()
+
+
+func _steer_toward(target_direction: Vector3, input_strength: float, delta: float) -> void:
+	var target_angle := atan2(-target_direction.x, -target_direction.z)
+	var angle_to_target := absf(
+		wrapf(target_angle - visual.rotation.y, -PI, PI)
+	)
+	var turn_in_place_threshold := deg_to_rad(turn_in_place_angle)
+
+	if angle_to_target >= turn_in_place_threshold:
+		is_turning_in_place = true
+
+	if is_turning_in_place:
+		var pivot_step := deg_to_rad(turn_in_place_speed) * delta
+		visual.rotation.y = rotate_toward(visual.rotation.y, target_angle, pivot_step)
+
+		angle_to_target = absf(
+			wrapf(target_angle - visual.rotation.y, -PI, PI)
+		)
+		if angle_to_target >= turn_in_place_threshold:
+			return
+
+		is_turning_in_place = false
+
+	# Moving along the current facing direction while rotating produces an arc.
+	# angular_speed = linear_speed / radius, so turn_radius is measured in meters.
+	var current_speed := move_speed * input_strength
+	var turn_speed := current_speed / maxf(turn_radius, 0.001)
+	visual.rotation.y = rotate_toward(
+		visual.rotation.y,
+		target_angle,
+		turn_speed * delta
+	)
+
+	var facing_direction := -visual.global_basis.z
+	facing_direction.y = 0.0
+	facing_direction = facing_direction.normalized()
+	velocity.x = facing_direction.x * current_speed
+	velocity.z = facing_direction.z * current_speed
 
 
 func _get_input_vector() -> Vector2:
